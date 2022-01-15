85ebc589c99e36f00724abcf4095a874a7ed1de7
ada <moimoi870416@gmail.com>
2022-01-16 01:15:57 +0800

    [KataTest]String Average

---
+++ KataTest/KataTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataTest
+{
+    public class Program
+    {
+        public Dictionary<string, int> ChangeTypeDictionary = new Dictionary<string, int>()
+            {
+                { "one", 1 },
+                { "two", 2},
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven",7 },
+                { "eight", 8 },
+                { "nine", 9 },
+                { "zero", 0 }
+            };
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Console.WriteLine(program.StringAverage(""));
+        }
+        public string StringAverage(string input)
+        {
+            if (input == "") return "n/a";
+
+            var numbers = input.Split(" ");
+
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                if (!ChangeTypeDictionary.ContainsKey(number)) return "n/a";
+                sum += ChangeTypeDictionary[number];
+            }
+            sum /= numbers.Length;
+            return ChangeTypeDictionary.FirstOrDefault(x => x.Value == sum).Key;
+        }
+    }
+}
---
+++ KataTest/KataTestTests/ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTest.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        public Program Program = new Program();
+        [TestMethod()]
+        public void StringAverage_enter_zero_nine_five_two_four_get_four()
+        {
+
+            Assert.AreEqual("four", Program.StringAverage("zero nine five two"));
+        }
+        [TestMethod()]
+        public void StringAverage_enter_empty_get_na()
+        {
+            Assert.AreEqual("n/a", Program.StringAverage(""));
+
+        }
+        [TestMethod()]
+        public void  StringAverage_enter_one_ten_four_get_na()
+        {
+            Assert.AreEqual("n/a", Program.StringAverage("one ten"));
+        }
+        [TestMethod()]
+        public void StringAverage_enter_two_apple_four_get_na()
+        {
+
+            Assert.AreEqual("n/a", Program.StringAverage("two apple"));
+        }
+    }
+}
